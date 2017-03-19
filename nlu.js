8c372235137922aeebe839e488bfd2bcb4449487
snta <snta@github.com>
2017-03-18 22:15:55 -0700

    nlu

--- nlu.js
+++ nlu.js
@@ -1,18 +1,19 @@
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var properties = require('properties');
 
-var nlu_user = "PLACEHOLDER";
+var nlu_user = 'PLACEHOLDER';
 var nlu_pass = 'PLACEHOLDER';
 
 properties.parse("./watson_creds.properties", { path : true, sections: true }, function (error, obj) {
   if (error) return console.error (error);
-  console.log(obj.nlu.username); 
 
   nlu_user = obj.nlu.username;
   nlu_pass = obj.nlu.password;
 });
 
-console.log("asdf " + nlu_user);
+console.log("value of nlu_user:\n Expected: watson_user; Actual: " + nlu_user);
+
+/*
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'username': nlu_user,
   'password': nlu_pass,
@@ -29,11 +30,10 @@ var parameters = {
     }
   }
 }
-
-console.log(nlu_user);
 natural_language_understanding.analyze(parameters, function(err, response) {
   if (err)
-    console.log('error:', err);
+    console.log('error:');
   else
     console.log(JSON.stringify(response, null, 2));
 });
+*/
